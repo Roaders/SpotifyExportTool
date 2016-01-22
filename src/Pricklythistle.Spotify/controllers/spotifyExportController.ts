046061a84e683aa42cf6c0fbf595ee053808793a
Giles Roadnight <giles@roadnight.name>
2016-01-22 11:52:34 +0000

    initial code

---
+++ src/Pricklythistle.Spotify/controllers/spotifyExportController.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Giles on 22/01/2016.
+ */
+
+module Pricklythistle.Spotify.Controllers {
+
+    export class SpotifyExportController {
+
+        message: string = "hello from controller";
+
+    }
+
+}
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+/**
+ * Created by Giles on 22/01/2016.
+ */
+
+///<reference path="Pricklythistle.Spotify/controllers/spotifyExportController.ts" />
